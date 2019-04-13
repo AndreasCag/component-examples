a5c446eca1513230a4dad26047b2513b1df083b5
andreascag <andreas97fm@gmail.com>
2019-04-13 17:47:44 +0300

    feat(notifications): enable notifications plugin

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import notificationPlugin from '@/plugins/notification';
+import modalPlugin from '@/plugins/modal';
 
 Vue.config.productionTip = false;
 
 Vue.use(notificationPlugin);
+Vue.use(modalPlugin);
 
 new Vue({
   router,
