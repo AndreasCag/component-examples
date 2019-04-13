0e648e14e4463123efa25f889e996ecf1d6e98ad
andreascag <andreas97fm@gmail.com>
2019-04-13 17:44:19 +0300

    chore: add bundle analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   lintOnSave: false,
+  configureWebpack: (config) => {
+    // eslint-disable-next-line no-param-reassign
+    config.devtool = false;
+  },
+  pluginOptions: {
+    webpackBundleAnalyzer: {
+      openAnalyzer: false,
+    },
+  },
 };
