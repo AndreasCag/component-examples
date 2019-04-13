b4fd93a2dc233e8b813c734512db59dc1564034d
andreascag <andreas97fm@gmail.com>
2019-04-13 11:59:49 +0300

    chore: add base pages

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
 
 Vue.use(Router);
 
@@ -8,16 +7,19 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      redirect: '/notification',
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/notification',
+      component: () => import(/* webpackChunkName: "notification" */ './views/Notification.vue'),
+    },
+    {
+      path: '/table',
+      component: () => import(/* webpackChunkName: "table" */ './views/Table.vue'),
+    },
+    {
+      path: '/modal',
+      component: () => import(/* webpackChunkName: "modal" */ './views/Modal.vue'),
     },
   ],
 });
---
+++ src/views/Notification.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    Notification
+    <button @click="addNotification">
+      Add notification
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+
+  methods: {
+
+    addNotification() {
+      this.$addNotification({
+        header: 'Test Test Test Test Test Test Test Test Test ',
+        content: 'Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text ',
+        duration: 40000000,
+      });
+    },
+
+  },
+
+};
+</script>
