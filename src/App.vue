ca1cdc57fcba062b79ab90fe41e77e638a2d1ab0
andreascag <andreas97fm@gmail.com>
2019-04-13 12:00:56 +0300

    chore: add links to base pages

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/table">Table</router-link>
+      |
+      <router-link to="/notification">Notification</router-link>
+      |
+      <router-link to="/modal">Modal</router-link>
+      |
+      <router-link to="/tooltip">Tooltip</router-link>
+      |
+      <router-link to="/parallax">Parallax</router-link>
+      |
+      <router-link to="/calendar">Calendar</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
+body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 #nav {
   padding: 30px;
+  text-align: center;
+  color: #2c3e50;
 }
 
 #nav a {
