7e297b469cb1613a50974cc28b7e76f4974cd46e
andreascag <andreas97fm@gmail.com>
2019-04-13 12:00:08 +0300

    feat(notifiactions): enable notifications plugin

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import notificationPlugin from '@/plugins/notification';
 
 Vue.config.productionTip = false;
+
+Vue.use(notificationPlugin);
 
 new Vue({
   router,
